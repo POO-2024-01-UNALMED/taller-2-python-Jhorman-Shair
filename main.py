545955ab535ae1d03ff94df4fa1aa579e74c3e03
Jhorman-Shair <jhramirezhe@unal.edu.co>
2024-02-17 16:22:34 -0500

    Primeros cambios
    Darle revision por el bot

---
+++ main.py
@@ -0,0 +1,55 @@
+class Asiento:
+    def __init__(self,color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    
+    def cambiarColor(self,x):
+        lista=["rojo","verde","amarillo","negro","blanco"]
+        for i in lista:
+            if i==x:
+                self.color=x
+
+class Motor:
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+        
+    def cambiarRegistro(self, y):
+        if type(y)==int:
+            self.registro=y
+            
+    def asignarTipo(self,z):
+        modelos=["electrico","gasolina"]
+        for i in modelos:
+            if i==z:
+                self.tipo=z
+
+class Auto:
+    def __init__(self,modelo,precio,asientos,marca,motor,registro,cantidadCreados):
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+        self.cantidadCreados=cantidadCreados
+        
+    def cantidadAsientos(self):
+        conteo=0
+        for i in self.asientos:
+            if i!=None:
+                conteo+=1
+        return conteo
+    
+    def verificarIntegridad(self):
+        resultado=""
+        if self.motor.registro==self.registro:
+            resultado="Auto original"
+            for i in self.asientos:
+                if i.registro!=self.registro:
+                    resultado="Las piezas no son originales"
+        else:
+            resultado="Las piezas no son originales"
+        return resultado
